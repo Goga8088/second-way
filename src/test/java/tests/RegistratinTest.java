497bf294af3f946aa56f7bbcaea1b9f252e0ff4b
Goga8088 <obuhov.gosha@gmail.com>
2025-04-07 23:33:27 +0500

    HomeWork

--- src/test/java/tests/RegistratinTest.java
+++ src/test/java/tests/RegistratinTest.java
@@ -4,9 +4,13 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
+import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static java.awt.SystemColor.text;
 
 public class RegistratinTest {
 
@@ -29,20 +33,33 @@ public class RegistratinTest {
             open("/automation-practice-form");
             $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
 
+            executeJavaScript("$('footer').remove()");
+
             $("#firstName").setValue(userName);
             $("#lastName").setValue("Egorov");
             $("#userEmail").setValue("egorov@test.ru");
+            $("#genterWrapper").$(byText("Other")).click();
             $("#userNumber").setValue("1234567890");
+
+
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("July");
+            $(".react-datepicker__year-select").selectOption("1998");
+            $(".react-datepicker__day--030:not(.react-datepicker__day--030 react-datepicker__day--outside-month)").click();
+            $("#subjectsInput").setValue("Math").pressEnter();
+            $("#hobbiesWrapper").$(byText("Sports")).click();
+            $("#uploadPicture").uploadFromClasspath("img/1.png");
+            //$("#uploadPicture").uploadFile( new File("src/test/resources/img/1.png"));
             $("#currentAddress").setValue("Some address 1");
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText("NCR")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Delhi")).click();
+            $("#submit").click();
 
+            $(".modal-dialog").should(appear);
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            $(".table-responsive").shouldHave(text(userName), text("Egorov"), text("egorov@test.ru"), text("1234567890"));
 
-
-
-
-            //$("#output #name").shouldHave(text("Alex")).pressEnter();
-           // $("#output #email").shouldHave(text("alex@alex.fgf")).pressEnter();
-           // $("#output #currentAddress").shouldHave(text("Some street 1")).pressEnter();
-           // $("#output #permanentAddress").shouldHave(text("Another street 2")).pressEnter();
-        }
 }
 
